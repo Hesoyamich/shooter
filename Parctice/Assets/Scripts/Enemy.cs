cc0cca45f2c524661c444d9716a6bc6336afbdc8
Hesoyamich <48620599+Hesoyamich@users.noreply.github.com>
2022-06-01 18:12:24 +0300

    Базированная атака у противников
    Нужно сделать какую-нибудь анимацию и полноценную смерть игрока

--- Parctice/Assets/Scripts/Enemy.cs
+++ Parctice/Assets/Scripts/Enemy.cs
@@ -7,13 +7,18 @@ public class Enemy : MonoBehaviour
 {
 
     public int hp = 100;
+    public int damage = 20;
     public NavMeshAgent agent;
     
     public Transform player;
     
+    public LayerMask playerLayerMask;
+    public float attackCooldown;
     bool moving;
-  
+    bool attacking;
+
     void Awake() {
+
     }
 
     void Update()
@@ -24,12 +29,13 @@ public class Enemy : MonoBehaviour
         {
             moving = false;
             agent.SetDestination(transform.position);
+            transform.LookAt(player);
+            if (!attacking)
+                StartCoroutine(AttackPlayer());
         } 
 
-        if (moving)
+        if (moving && !attacking)
             agent.SetDestination(player.position);
-
-
 
         if (hp <= 0){
             Destroy(gameObject);
@@ -39,5 +45,17 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(int damage) 
     {
         hp -= damage;
+    }
+
+    IEnumerator AttackPlayer()
+    {
+        if (Physics.CheckSphere(transform.position + transform.forward, 1f, playerLayerMask))
+        {
+            attacking = true;
+            player.parent.gameObject.GetComponent<PlayerMovement>().GetDamage(damage);
+            Debug.Log(player.parent.gameObject.GetComponent<PlayerMovement>().hp);
+            yield return new WaitForSeconds(attackCooldown);
+            attacking = false;
+        }
     }
 }
